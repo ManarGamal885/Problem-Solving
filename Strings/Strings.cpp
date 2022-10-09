79369131a6ee714fe2457433e5add219f182d9b6
Manar <manarg281@gmail.com>
2022-10-09 16:45:05 +0200

    `

---
+++ Strings/Strings.cpp
@@ -0,0 +1,22 @@
+//https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+void init ()
+{
+    cin.tie (0);
+    cin.sync_with_stdio (0);
+}
+void solve()
+{
+    string s,t;
+    cin>>s>>t;
+    cout<<s.length()<<" "<<t.length()<<endl;
+    cout<<s<<" "<<t<<endl;
+}
+
+
+int main ()
+{
+    init();
+    solve();
+}
