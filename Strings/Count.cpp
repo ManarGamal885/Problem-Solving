928a503b006d89abb5654cfc98d61ec1b56d4300
Manar <manarg281@gmail.com>
2022-10-09 17:02:23 +0200

    `

---
+++ Strings/Count.cpp
@@ -0,0 +1,68 @@
+//https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/E
+#include <bits/stdc++.h>
+using namespace std;
+void init ()
+{
+    cin.tie (0);
+    cin.sync_with_stdio (0);
+}
+void solve()
+{
+    long long sum=0;
+    string s;
+    cin>>s;
+    for(int i=0; i<s.length(); i++){
+        sum+= s[i]-'0';
+    }
+    cout<<sum<<endl;
+    /*long long k,sum=0;
+    cin>>k;
+    while(k !=0){
+        int d= k%10;
+        sum+=d;
+        k/=10;
+    }
+    cout<<sum<<endl;*/
+}
+
+
+int main ()
+{
+    init();
+    solve();
+
+}
+//https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/E
+#include <bits/stdc++.h>
+using namespace std;
+void init ()
+{
+    cin.tie (0);
+    cin.sync_with_stdio (0);
+}
+void solve()
+{
+    long long sum=0;
+    string s;
+    cin>>s;
+    for(int i=0; i<s.length(); i++){
+        sum+= s[i]-'0';
+    }
+    cout<<sum<<endl;
+    /*long long k,sum=0;
+    cin>>k;
+    while(k !=0){
+        int d= k%10;
+        sum+=d;
+        k/=10;
+    }
+    cout<<sum<<endl;*/
+}
+
+
+int main ()
+{
+    init();
+    solve();
+
+}
