92aed5fe7956eef834682fabd35fd81d57f48940
Manar <manarg281@gmail.com>
2022-10-20 22:56:06 +0200

    ~

--- Learning/findPrimesAndCheck.cpp
+++ Learning/findPrimesAndCheck.cpp
@@ -30,34 +30,75 @@ bool checkPrime(int n)
     }
     return true;
 }
+
+
 //finding the primes in O(n*log(log(n)))
-vector<int> findAllPrimes(int n){
+vector<int> findAllPrimes(int n)
+{
     vector<int>v;
- bool primes[n+1];
- memset(primes, true, n+1);
- for(int i=2; i <n/2; i++ ){
-    if(primes[i/2]){
-        for(int j = i*i; j<=n; j+=i){
-            primes[j]=false;
+    bool primes[n+1];
+    memset(primes, true, n+1);
+//instade of making i*i <=n we make i <= n/2
+//Q: why i<=n/2
+   // primes[0]=primes[1]=false;
+    for(int i=2; i <=n/2; i++ )
+    {
+        if(primes[i/2])
+        {
+            for(int j = i*i; j<=n; j+=i)
+            {
+                primes[j]=false;
+            }
         }
     }
- }
- for(int i=2; i<=n; i++){
-    if(primes[i]){
-        v.push_back(i);
+    for(int i=2; i<=n; i++)
+    {
+        if(primes[i])
+        {
+            v.push_back(i);
+        }
     }
- }
- return v;
+    return v;
+}
+
+
+//finding the primes in O(n*log(log(n)))
+vector<int> findAllPrimes2(int n)
+{
+    vector<int>v;
+    bool primes[n+1];
+    memset(primes, true, n+1);
+//instade of making i*i <=n we make i <= n/2
+    primes[0]=primes[1]=false;
+    for(int i=2; i <=n/2; i++ )
+    {
+        if(primes[i])
+        {
+            for(int j = i*i; j<=n; j+=i)
+            {
+                primes[j]=false;
+            }
+        }
+    }
+    for(int i=2; i<=n; i++)
+    {
+        if(primes[i])
+        {
+            v.push_back(i);
+        }
+    }
+    return v;
 }
 
 
 int main ()
 {
-   int n;
-   cin>>n;
-   vector<int> v;
-   v = findAllPrimes(n);
-   for(int i=0; i<v.size(); i++){
-    cout<<v[i]<<" ";
-   }
+    int n;
+    cin>>n;
+    vector<int> v;
+    v = findAllPrimes(n);
+    for(int i=0; i<v.size(); i++)
+    {
+        cout<<v[i]<<" ";
+    }
 }
