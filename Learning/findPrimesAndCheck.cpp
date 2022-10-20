835363ec3ee927e0819c20e9f99b93739fc3623a
Manar <manarg281@gmail.com>
2022-10-20 22:53:42 +0200

    `

---
+++ Learning/findPrimesAndCheck.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*void init ()
+{
+    cin.tie (0);
+    cin.sync_with_stdio (0);
+}*/
+
+//check if rime in sqrt(n)
+bitset<500001> Primes;
+bool checkPrime(int n)
+{
+    if(n<2)
+    {
+        return false;
+    }
+    else if(n%2==0)
+    {
+        return false;
+    }
+    else
+    {
+        for(int i=3; i<=n/i; i+=2)
+        {
+            if(n%i==0)
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+//finding the primes in O(n*log(log(n)))
+vector<int> findAllPrimes(int n){
+    vector<int>v;
+ bool primes[n+1];
+ memset(primes, true, n+1);
+ for(int i=2; i <n/2; i++ ){
+    if(primes[i/2]){
+        for(int j = i*i; j<=n; j+=i){
+            primes[j]=false;
+        }
+    }
+ }
+ for(int i=2; i<=n; i++){
+    if(primes[i]){
+        v.push_back(i);
+    }
+ }
+ return v;
+}
+
+
+int main ()
+{
+   int n;
+   cin>>n;
+   vector<int> v;
+   v = findAllPrimes(n);
+   for(int i=0; i<v.size(); i++){
+    cout<<v[i]<<" ";
+   }
+}
